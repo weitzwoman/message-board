ba4e751c739a47771245648a8cfbd31c638597be
Tracie Weitzman <tlweitzman@gmail.com>
2017-01-13 13:25:59 -0800

    add answers to question page

--- app/models/answer.js
+++ app/models/answer.js
@@ -1,8 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  author: DS.attr();
-  date: DS.attr();
-  content: DS.attr()
+  author: DS.attr(),
+  date: DS.attr(),
+  content: DS.attr(),
   message: DS.belongsTo('message', { async: true})
 });
